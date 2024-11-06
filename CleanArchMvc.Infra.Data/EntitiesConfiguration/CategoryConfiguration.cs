8ab57bc1ca2573bd8c088f1b6f77499add88a388
Ruilon <ruilonbl@gmail.com>
2024-11-05 21:49:05 -0300

    finalizando a implementação da camada de infraestrutura e criando as migrações

--- CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -15,6 +15,12 @@ namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletronicos"),
+                new Category(3, "Acessórios")
+                );
         }
     }
 }
--- CleanArchMvc.WebUI/Program.cs
+++ CleanArchMvc.WebUI/Program.cs
@@ -1,5 +1,6 @@
 using CleanArchMvc.Infra.Data.Context;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
+using CleanArchMvc.Infra.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
